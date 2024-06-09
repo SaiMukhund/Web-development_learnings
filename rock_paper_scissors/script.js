830aeff5ffada2b48de531483c70942ef4e5d56a
SaiMukhund <saimukhund2001@gmail.com>
2024-06-09 21:50:09 +0530

    wrote html and css code , need to add js write

---
+++ rock_paper_scissors/script.js
@@ -0,0 +1,8 @@
+let imgHover=document.querySelectorAll("selector");
+let userScore=document.querySelector("#user-score");
+let compScore=document.querySelector("#comp-score");
+let resultText=document.querySelector("#result");
+
+imgHover.addEventListener("mouseover", ()=>{
+    imgHover.style="border-style:solid; border-width:2px;"
+})
