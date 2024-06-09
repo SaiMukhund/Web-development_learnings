bba1ce8f8db15c75717f179b12d2ac2e1cf52a5e
SaiMukhund <saimukhund2001@gmail.com>
2024-06-09 22:06:01 +0530

    added functionality for hovering

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -1,8 +1,14 @@
-let imgHover=document.querySelectorAll("selector");
+let imgHover=document.querySelectorAll(".selector");
 let userScore=document.querySelector("#user-score");
 let compScore=document.querySelector("#comp-score");
 let resultText=document.querySelector("#result");
 
-imgHover.addEventListener("mouseover", ()=>{
-    imgHover.style="border-style:solid; border-width:2px;"
-})
+imgHover.forEach((val)=>{
+    val.addEventListener("mouseover",()=>{
+        val.classList.add("hovered");
+    });
+    val.addEventListener("mouseout",()=>{
+        val.classList.remove("hovered");
+    });
+}
+);
