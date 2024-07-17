1d6cc271ba3a6f4ac49d927a0127c676c9f97522
SaiMukhund <saimukhund2001@gmail.com>
2024-07-17 22:36:01 +0530

    completed the rock paper scissors

--- rock_paper_scissors/script.js
+++ rock_paper_scissors/script.js
@@ -1,11 +1,7 @@
 let imgHover=document.querySelectorAll(".selector");
 let userScore=document.querySelector("#user-score");
 let compScore=document.querySelector("#comp-score");
-let resultText=document.querySelector("#result");
-let paper=document.querySelector("#paper");
-let scissor=document.querySelector("#scissor");
-let rock=document.querySelector("#rock");
-console.log(rock);
+let resultText=document.querySelector(".result");
 imgHover.forEach((val)=>{
     val.addEventListener("mouseover",()=>{
         val.classList.add("hovered");
@@ -15,6 +11,12 @@ imgHover.forEach((val)=>{
     });
 }
 );
+
+let paper=document.querySelector("#paper");
+let scissor=document.querySelector("#scissor");
+let rock=document.querySelector("#rock");
+console.log(rock);
+
 let user_score=0;
 let comp_score=0;
 function getRandomNumber(){
@@ -25,12 +27,11 @@ rock.addEventListener("click",()=>{
     let num=getRandomNumber();
     if (num===0){
         resultText.innerHTML="Draw, play again !!";
-        resultText.
     }
     else if(num===1){
         comp_score+=1;
         compScore.innerHTML=comp_score;
-        resultText.innerHTML="The computer Won against you";
+        resultText.innerHTML="The computer Won, Paper beats the Rock";
         if (resultText.classList.contains("won")) {
             resultText.classList.remove("won");
         }
@@ -40,10 +41,65 @@ rock.addEventListener("click",()=>{
     else{
         user_score+=1;
         userScore.innerHTML=user_score;
-        resultText.innerHTML="You have won against the Computer";
+        resultText.innerHTML="You have won, Rock beats the Scissors ";
         if (resultText.classList.contains("lost")) {
             resultText.classList.remove("lost");
         }
         resultText.classList.add("won");
+        console.log(resultText);
+    }
+});
+
+paper.addEventListener("click",()=>{
+    let num=getRandomNumber();
+    if (num===1){
+        resultText.innerHTML="Draw, play again !!";
+    }
+    else if(num===2){
+        comp_score+=1;
+        compScore.innerHTML=comp_score;
+        resultText.innerHTML="The computer Won, Scissors beats the Paper";
+        if (resultText.classList.contains("won")) {
+            resultText.classList.remove("won");
+        }
+        resultText.classList.add("lost");
+        
+    }
+    else{
+        user_score+=1;
+        userScore.innerHTML=user_score;
+        resultText.innerHTML="You have won, Rock beats the Scissors ";
+        if (resultText.classList.contains("lost")) {
+            resultText.classList.remove("lost");
+        }
+        resultText.classList.add("won");
+        console.log(resultText);
+    }
+});
+
+scissor.addEventListener("click",()=>{
+    let num=getRandomNumber();
+    if (num===2){
+        resultText.innerHTML="Draw, play again !!";
+    }
+    else if(num===0){
+        comp_score+=1;
+        compScore.innerHTML=comp_score;
+        resultText.innerHTML="The computer Won, Rock beats the Scissors";
+        if (resultText.classList.contains("won")) {
+            resultText.classList.remove("won");
+        }
+        resultText.classList.add("lost");
+        
+    }
+    else{
+        user_score+=1;
+        userScore.innerHTML=user_score;
+        resultText.innerHTML="You have won, Scissor beats the Paper ";
+        if (resultText.classList.contains("lost")) {
+            resultText.classList.remove("lost");
+        }
+        resultText.classList.add("won");
+        console.log(resultText);
     }
 })
